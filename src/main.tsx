918635979d1c68237fdf75bf4fa76d302964d429
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-06-21 21:14:25 +0200

    Configuring Apollo

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
